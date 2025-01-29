da4d880e323dda2c067b078c97ba8f5dfc789a12
Mohammad Hossain Verdian <107686425+mahvxxx@users.noreply.github.com>
2025-01-29 14:57:47 +0330

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
-# Install the required Python libraries
-pip install -r requirements.txt
+
+# Specify the URL of the GitHub repository (or any other URL where you have uploaded the files)
+echo "Downloading program files from GitHub repository..."
+git clone https://github.com/yourusername/yourrepository.git
+
+# Navigate into the project directory
+cd yourrepository
+
+# Update package list and install pip (if not installed)
+echo "Updating package list and installing pip..."
+sudo apt-get update
+sudo apt-get install python3-pip -y
+
+# Install the required Python libraries from requirements.txt
+echo "Installing required Python libraries from requirements.txt..."
+pip3 install -r requirements.txt
 
 # Run the Python script
-python wallets_founder.py
+echo "Running the Python script..."
+python3 wallet-founder.py
